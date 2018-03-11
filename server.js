24aeb5f02409c8e9245436341fbf9a96f54df5c5
OtherwiseSamwise <37043321+OtherwiseSamwise@users.noreply.github.com>
2018-03-11 14:51:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/a1',function(req,res)
+{res.send("another one");
+    
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
